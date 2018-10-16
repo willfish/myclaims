9e1e1be3d25691a2a38e265c6a1fc7bfccdd76aa
William Fish <william.michael.fish@gmail.com>
2018-10-16 13:48:13 +0100

    Adds mutations for states of a claim which we can subscribe to later

--- lib/myclaims_web/resolvers/claims.ex
+++ lib/myclaims_web/resolvers/claims.ex
@@ -6,4 +6,31 @@ defmodule Resolvers.Claims do
   def claim(_, %{id: id}, _) do
     {:ok, Myclaims.Insurance.get_claim!(id)}
   end
+
+  def new_claim(_, _, _) do
+
+  end
+
+  def reported(_, %{id: id}, _) do
+    {:ok, update_state(id, "REPORTED")}
+  end
+
+  def with_underwriter(_, %{id: id}, _) do
+    {:ok, update_state(id, "WITH_LOSS_ADJUSTER")}
+  end
+
+  def with_assessor(_, %{id: id}, _) do
+    {:ok, update_state(id, "WITH_LOSS_ASSESSOR")}
+  end
+
+  def completed(_, %{id: id}, _) do
+    {:ok, update_state(id, "COMPLETED")}
+  end
+
+  defp update_state(id, state) do
+    claim = Myclaims.Insurance.get_claim!(id)
+    attrs = %{state: state}
+    {:ok, claim} = Myclaims.Insurance.update_claim(claim, attrs)
+    claim
+  end
 end
