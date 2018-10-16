e38f45f868868b16dae9ab4f42c6a80fdc5be6f2
William Fish <william.michael.fish@gmail.com>
2018-10-16 22:06:04 +0100

    Implement Absinthe.Phoenix.Socket macro

--- lib/myclaims_web/channels/user_socket.ex
+++ lib/myclaims_web/channels/user_socket.ex
@@ -1,5 +1,6 @@
 defmodule MyclaimsWeb.UserSocket do
   use Phoenix.Socket
+  use Absinthe.Phoenix.Socket, schema: PlateSlateWeb.Schema
 
   ## Channels
   # channel "room:*", MyclaimsWeb.RoomChannel
