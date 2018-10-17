451a871e5c6b9010df796203abbe96f013403dc8
William Fish <william.michael.fish@gmail.com>
2018-10-17 15:03:24 +0100

    Adds claim javascript for claims subscriptions

---
+++ assets/js/claim.js
@@ -0,0 +1,20 @@
+// import apollo_client from "./apollo_client"
+
+// Subscribe to changes in claims
+// Update the DOM when the subscription yields a result
+//
+
+// class HelloMessage extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello {this.props.name}
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <HelloMessage name="Taylor" />,
+//   document.getElementById('example')
+// );
